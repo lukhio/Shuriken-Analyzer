ef4dbfdb26cb7c0bb47e1893a48d31b5b4031b05
Julien Gamba <julien@jgamba.eu>
2024-03-12 22:38:36 +0100

    Some more tests of the core API (WIP)

--- shuriken/include/shuriken/api/Rust/src/lib.rs
+++ shuriken/include/shuriken/api/Rust/src/lib.rs
@@ -13,9 +13,51 @@ mod tests {
 
     #[test]
     fn check_parse_dex() {
-        let c_str = CString::new("/home/jgamba/dev/Shuriken-Analyzer/shuriken/tests/compiled/DexParserTest.dex").unwrap();
-        let c_world = c_str.as_ptr() as *const c_char;
-        let dex = unsafe { parse_dex(c_world); };
-        assert_eq!(true, true)
+        unsafe {
+            let c_str = CString::new("/home/jgamba/dev/Shuriken-Analyzer/shuriken/tests/compiled/DexParserTest.dex").unwrap();
+            let c_world = c_str.as_ptr() as *const c_char;
+            let dex =  parse_dex(c_world);
+            assert_eq!(dex.is_null(), false);
+        }
+    }
+
+    #[test]
+    fn check_get_number_of_strings() {
+        unsafe {
+            let c_str = CString::new("/home/jgamba/dev/Shuriken-Analyzer/shuriken/tests/compiled/DexParserTest.dex").unwrap();
+            let c_world = c_str.as_ptr() as *const c_char;
+            let dex =  parse_dex(c_world);
+            assert_eq!(get_number_of_strings(dex), 33);
+        }
+    }
+
+    #[test]
+    fn check_get_string_by_id() {
+        todo!();
+    }
+
+    #[test]
+    fn check_get_number_of_classes() {
+        unsafe {
+            let c_str = CString::new("/home/jgamba/dev/Shuriken-Analyzer/shuriken/tests/compiled/DexParserTest.dex").unwrap();
+            let c_world = c_str.as_ptr() as *const c_char;
+            let dex =  parse_dex(c_world);
+            assert_eq!(get_number_of_classes(dex), 1);
+        }
+    }
+
+    #[test]
+    fn check_get_class_by_id() {
+        todo!();
+    }
+
+    #[test]
+    fn check_get_class_by_name() {
+        assert!(true);
+    }
+
+    #[test]
+    fn check_get_method_by_name() {
+        todo!();
     }
 }
