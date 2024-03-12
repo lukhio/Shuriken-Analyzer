f4ee55ff3de0913fb7cae50b0864a214ce4c4c26
Julien Gamba <julien@jgamba.eu>
2024-03-12 21:40:43 +0100

    Generate bindings at build time

---
+++ shuriken/include/shuriken/api/Rust/build.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    // println!("cargo:rustc-link-search={}", env::var("LIB_LINK_SEARCH").unwrap());
+    println!("cargo:rustc-link-search=/home/jgamba/dev/Shuriken-Analyzer/build/shuriken/");
+
+    // Tell cargo to tell rustc to link the shuriken lib
+    println!("cargo:rustc-link-lib=shuriken");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("../C/shuriken_core.h")
+        .clang_arg("-std=c++17")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        // .clang_args(&["-std=c++17"])
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/shuriken_core.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("shuriken_core.rs"))
+        .expect("Couldn't write bindings!");
+}
